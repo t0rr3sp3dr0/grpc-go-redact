352372359a6393c99c0688b078b92e416dfc9ec7
Sam Kreter <samkreter@gmail.com>
2021-03-12 15:09:04 -0700

    updating struct to allow for better testing

--- generate.go
+++ generate.go
@@ -1,5 +1,103 @@
 package main
 
-func GenerateStringFunc() error {
+import (
+	"errors"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"strconv"
 
+	"golang.org/x/tools/go/ast/astutil"
+)
+
+const (
+	stringFuncMethodName = "String"
+	stringFuncGenFile    = "./gen/stringfunc.go"
+)
+
+func GenerateStringFunc(fset *token.FileSet, f *ast.File) error {
+	genStringFunc, requiredImports, err := getStringFuncASTNode(stringFuncGenFile)
+	if err != nil {
+		return err
+	}
+
+	// Add required imports from the pre-gen file
+	for _, reqiredImportPath := range requiredImports {
+		astutil.AddImport(fset, f, reqiredImportPath)
+	}
+
+	astutil.Apply(f, func(cr *astutil.Cursor) bool {
+		funcDecal, ok := cr.Node().(*ast.FuncDecl)
+		if !ok {
+			return true
+		}
+		if funcDecal.Name.String() != stringFuncMethodName {
+			return true
+		}
+
+		if len(funcDecal.Recv.List) != 1 {
+			log.Fatal("invalid number of recievers")
+		}
+
+		genStringFunc.Recv = funcDecal.Recv
+		// TODO: Allow for comments to be handled correctly
+		genStringFunc.Doc = nil
+
+		cr.Replace(genStringFunc)
+		return false
+	}, nil)
+
+	return nil
+}
+
+func getStringFuncASTNode(genfile string) (*ast.FuncDecl, []string, error) {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, genfile, nil, parser.ParseComments)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var stringFuncNode *ast.FuncDecl
+
+	requiredImports, err := getRequiredImports(fset, f)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	astutil.Apply(f, func(cr *astutil.Cursor) bool {
+		funcDecal, ok := cr.Node().(*ast.FuncDecl)
+		if !ok {
+			return true
+		}
+		if funcDecal.Name.String() == stringFuncMethodName {
+			stringFuncNode = funcDecal
+			return false
+		}
+
+		return true
+	}, nil)
+
+	if stringFuncNode == nil {
+		return nil, nil, errors.New("Failed to find String Func")
+	}
+
+	return stringFuncNode, requiredImports, nil
+}
+
+func getRequiredImports(fset *token.FileSet, f *ast.File) ([]string, error) {
+	requiredImports := []string{}
+
+	importLists := astutil.Imports(fset, f)
+	for _, importList := range importLists {
+		for _, importObj := range importList {
+			path, err := strconv.Unquote(importObj.Path.Value)
+			if err != nil {
+				return nil, err
+			}
+			requiredImports = append(requiredImports, path)
+		}
+	}
+
+	return requiredImports, nil
 }
--- main.go
+++ main.go
@@ -2,29 +2,16 @@ package main
 
 import (
 	"flag"
-    "go/ast"
     "go/parser"
     "go/token"
-	"go/printer"
 	"log"
-	"os"
-	"errors"
-	"strconv"
-
-	"golang.org/x/tools/go/ast/astutil"
-)
-
-const (
-	stringMethodName = "String"
 )
 
 
 func main(){
 	var inputFile string
-	var genFile string
 	var outputFile string
 
-	flag.StringVar(&genFile, "gen", "./gen/stringfunc.go", "path to the gen file")
 	flag.StringVar(&inputFile, "input", "", "path to the input file")
 	flag.StringVar(&outputFile, "output", "", "path to the output file. If non specifid, will override the input file.")
 	flag.Parse()
@@ -33,17 +20,9 @@ func main(){
 		log.Fatal("input file is mandatory")
 	}
 
-	if len(genFile) == 0 {
-		log.Fatal("gen file is mandatory")
-	}
 
 	if len(outputFile) == 0 {
 		outputFile = inputFile
-	}
-
-	genStringFunc, requiredImports, err := getStringFuncASTNode(genFile)
-	if err != nil {
-		log.Fatal(err)
 	}
 
 	fset := token.NewFileSet()
@@ -52,82 +31,15 @@ func main(){
 		return
 	}
 
-	for _, reqiredImportPath := range requiredImports {
-		astutil.AddImport(fset, f, reqiredImportPath)
+	if err := GenerateStringFunc(fset, f); err != nil {
+		log.Fatal(err)
 	}
 
-	astutil.Apply(f, func(cr *astutil.Cursor) bool {
-		funcDecal, ok := cr.Node().(*ast.FuncDecl)
-		if !ok {
-			return true
-		}
-		if funcDecal.Name.String() != stringMethodName {
-			return true
-		}
-
-		if len(funcDecal.Recv.List) != 1 {
-			log.Fatal("invalid number of recievers")
-		}
-
-		genStringFunc.Recv = funcDecal.Recv
-		// TODO: Allow for comments to be handled correctly
-		genStringFunc.Doc = nil
-
-		cr.Replace(genStringFunc)
-		return false
-	}, nil )
-
-	if err := writeASTToFile(inputFile, fset, f); err != nil {
+	if err := writeASTToFile(outputFile, fset, f); err != nil {
 		log.Fatal(err)
 	}
 }
 
 
-func getStringFuncASTNode(genfile string) (*ast.FuncDecl, []string, error) {
-	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, genfile, nil, parser.ParseComments)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	var stringFuncNode *ast.FuncDecl
 
 
-	requiredImports := getRequiredImports(fset, f)
-	
-	astutil.Apply(f, func(cr *astutil.Cursor) bool {
-		funcDecal, ok := cr.Node().(*ast.FuncDecl)
-		if !ok {
-			return true
-		}
-		if funcDecal.Name.String() == stringMethodName {
-			stringFuncNode = funcDecal
-			return false
-		}
-
-		return true
-	}, nil )
-
-	if stringFuncNode == nil {
-		return nil, nil, errors.New("Failed to find String Func")
-	}
-
-	return stringFuncNode, requiredImports, nil
-}
-
-func getRequiredImports(fset *token.FileSet, f *ast.File) []string {
-	requiredImports := []string{}
-
-	importLists := astutil.Imports(fset, f)
-	for _, importList := range importLists {
-		for _, importObj := range importList {
-			path, err := strconv.Unquote(importObj.Path.Value)
-			if err != nil {
-				log.Fatal(err)
-			}
-			requiredImports = append(requiredImports, path)
-		}
-	}
-
-	return requiredImports
-}
